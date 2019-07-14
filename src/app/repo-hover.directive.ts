8889770e320137146ded8d300852aee8972e889e
Brian <brians931@gmail.com>
2019-07-14 18:03:32 +0300

    add a custom directive to highlight the repo name on mouse hover

--- src/app/repo-hover.directive.ts
+++ src/app/repo-hover.directive.ts
@@ -1,10 +1,24 @@
-import { Directive } from '@angular/core';
+;
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appRepoHover]'
 })
 export class RepoHoverDirective {
 
-  constructor() { }
+  constructor(private elem: ElementRef) {
+  }
+
+  @HostListener('mouseover') onmouseover() {
+    this.repoDeco('#fb3');
+  }
+
+  @HostListener('mouseout') onmouseout() {
+    this.repoDeco("#1ebba3");
+  }
+
+  private repoDeco(color: string) {
+    this.elem.nativeElement.style.color = color;
+  }
 
 }
